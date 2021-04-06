50e123488791729030a60a5f8399e9ca6348815f
aMahanna <anthony.mahanna@gmail.com>
2021-04-06 19:23:18 -0400

    fix: candidates val attribute

--- src/assets/candidates.ts
+++ src/assets/candidates.ts
@@ -1,16 +1,16 @@
 /* eslint-disable */
 const CandidatesData: any = {
-  PRES: [{ name: 'First Last', val: 'anon' }],
-  FNCE: [{ name: 'First Last', val: 'anon' }],
-  ACDM: [{ name: 'First Last', val: 'anon' }],
-  COMS: [{ name: 'First Last', val: 'anon' }],
-  INTR: [{ name: 'First Last', val: 'anon' }],
-  EXTR: [{ name: 'First Last', val: 'anon' }],
-  SOCL: [{ name: 'First Last', val: 'anon' }],
-  PHIL: [{ name: 'First Last', val: 'anon' }],
-  EQUT: [{ name: 'First Last', val: 'anon' }],
-  EXAF: [{ name: 'First Last', val: 'anon' }],
-  INTE: [{ name: 'First Last', val: 'anon' }],
+  PRES: [{ name: 'First Last', val: 'last' }],
+  FNCE: [{ name: 'First Last', val: 'last' }],
+  ACDM: [{ name: 'First Last', val: 'last' }],
+  COMS: [{ name: 'First Last', val: 'last' }],
+  INTR: [{ name: 'First Last', val: 'last' }],
+  EXTR: [{ name: 'First Last', val: 'last' }],
+  SOCL: [{ name: 'First Last', val: 'last' }],
+  PHIL: [{ name: 'First Last', val: 'last' }],
+  EQUT: [{ name: 'First Last', val: 'last' }],
+  EXAF: [{ name: 'First Last', val: 'last' }],
+  INTE: [{ name: 'First Last', val: 'last' }],
 };
 
 export default CandidatesData;
